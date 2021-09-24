4e45c18d7104233fc57a1f3a3413c6bff53dfaa8
Erik Murer <murer.erik@gmail.com>
2021-09-24 13:18:58 +0200

    rework on Person CRUD methods

--- src/main/java/com/safetyNet/safetyNetAlerts/controllers/PersonController.java
+++ src/main/java/com/safetyNet/safetyNetAlerts/controllers/PersonController.java
@@ -1,7 +1,11 @@
 package com.safetyNet.safetyNetAlerts.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +23,9 @@ import com.safetyNet.safetyNetAlerts.Views.EmailView;
 import com.safetyNet.safetyNetAlerts.Views.FireAddressView;
 import com.safetyNet.safetyNetAlerts.Views.PersonInfoView;
 import com.safetyNet.safetyNetAlerts.Views.ChildAlertView;
+import com.safetyNet.safetyNetAlerts.models.Firestation;
 import com.safetyNet.safetyNetAlerts.models.Person;
+import com.safetyNet.safetyNetAlerts.repositories.PersonRepository;
 import com.safetyNet.safetyNetAlerts.services.ChildAlertService;
 import com.safetyNet.safetyNetAlerts.services.EmailService;
 import com.safetyNet.safetyNetAlerts.services.FireAddressService;
@@ -30,7 +36,10 @@ import com.safetyNet.safetyNetAlerts.services.PersonService;
 public class PersonController {
 
 	@Autowired
-	private PersonService personService;
+	PersonService personService;
+	
+	@Autowired
+	PersonRepository personRepository;
 	
 	@Autowired
 	ChildAlertService childAlertService;
@@ -53,22 +62,28 @@ public class PersonController {
 	
 	// Endpoint delete a person selected by the firstname and lastname
 	@DeleteMapping("person/{firstname} {lastname}")
-	private void deletePerson(@PathVariable("firstname") String firstName, @PathVariable ("lastname") String lastName) {
+	private  ResponseEntity<HttpStatus> deletePerson(@PathVariable("firstname") String firstName, @PathVariable ("lastname") String lastName) {
 		personService.delete(firstName, lastName);
+		return new ResponseEntity<HttpStatus>(HttpStatus.ACCEPTED);
 	}
 
 	// Endpoint : add a new person
 	@PostMapping(value = "/person")
-	private int savePerson(@RequestBody Person person) {
+	private ResponseEntity<Person> savePerson(@RequestBody Person person) {
 		personService.savePerson(person);
-		return person.getId();
+		return  new ResponseEntity<Person>(person, HttpStatus.CREATED);
+
 	}
 	
 	//Endpoint : modify person (except for firstName and lastName...methods from "https://www.baeldung.com/rest-http-put-vs-post"
 	@PutMapping(value = "/person/update/{id}")
-	void modifyPerson(@RequestBody Person person, @PathVariable int id) {
+	private ResponseEntity<Person> modifyPerson(@RequestBody Person person, @PathVariable int id) {
+		Optional<Person> firestationOptional = personRepository.findById(id);
+		if (!firestationOptional.isPresent())
+			return ResponseEntity.notFound().build();
 		personService.modifyPerson(person, id);
 		personService.savePerson(person);
+		return ResponseEntity.noContent().build();
 	}
 	
 	// ChildAltertService
