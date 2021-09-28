00c8a3bd4a442f1be273258e3e3b743ab7fd6601
ErikM06 <murer.erik@gmail.com>
2021-09-28 15:11:06 +0200

    working on logging with logback

--- src/main/java/com/safetyNet/safetyNetAlerts/services/FireAddressService.java
+++ src/main/java/com/safetyNet/safetyNetAlerts/services/FireAddressService.java
@@ -3,7 +3,9 @@ package com.safetyNet.safetyNetAlerts.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.safetyNet.safetyNetAlerts.DTO.FireAddressDTO;
 import com.safetyNet.safetyNetAlerts.repositories.MedicalRecordRepository;
@@ -15,7 +17,9 @@ public class FireAddressService {
 	MedicalRecordRepository medicalRecordRepository;
 
 	public List<FireAddressDTO> fireAddressServiceByAddress(String address) {
-
+		
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
 		List<FireAddressDTO> fireAddressDTOls = medicalRecordRepository.getMedicalRecordByAddress(address);
 		return fireAddressDTOls;
 
