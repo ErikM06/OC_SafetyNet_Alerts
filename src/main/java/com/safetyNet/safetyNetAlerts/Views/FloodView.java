97aafd7f74f3816e6d830d786087b8a6175837f6
Erik Murer <murer.erik@gmail.com>
2021-11-12 09:47:31 +0100

    modified flood mapping view

--- src/main/java/com/safetyNet/safetyNetAlerts/Views/FloodView.java
+++ src/main/java/com/safetyNet/safetyNetAlerts/Views/FloodView.java
@@ -2,6 +2,7 @@ package com.safetyNet.safetyNetAlerts.Views;
 
 import com.safetyNet.safetyNetAlerts.Views.MedicalRecordView.Birthdate;
 import com.safetyNet.safetyNetAlerts.Views.MedicalRecordView.MedicationAndAllergie;
+import com.safetyNet.safetyNetAlerts.Views.PersonView.PersonAddress;
 import com.safetyNet.safetyNetAlerts.Views.PersonView.PersonEmail;
 import com.safetyNet.safetyNetAlerts.Views.PersonView.PersonFirstName;
 import com.safetyNet.safetyNetAlerts.Views.PersonView.PersonLastName;
@@ -10,7 +11,7 @@ import com.safetyNet.safetyNetAlerts.Views.PersonView.PersonPhone;
 
 public class FloodView {
 	
-	public interface floodView extends PersonFirstName, PersonPhone, PersonLastName, PersonEmail, MedicationAndAllergie, Birthdate {
+	public interface floodView extends PersonAddress, PersonFirstName, PersonPhone, PersonLastName, MedicationAndAllergie, Birthdate {
 		
 	}
 }
